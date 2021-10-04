c72dc9b7c6274e0f074934f3aecf60736a588be6
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-10-04 13:36:29 +0300

    added DetailViewController

---
+++ Petitions/DetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  Petitions
+//
+//  Created by Abdulrahman on 10/4/21.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let detailItem = detailItem else {
+            return
+        }
+        
+        let html = """
+<html>
+<head>
+<meta name="viewport" content="width=device-wdith, initial-scale=1">
+<style> body {font-size: 150%;} </style>
+</head>
+<body>
+\(detailItem.body)
+</body>
+<html>
+"""
+        webView.loadHTMLString(html, baseURL: nil)
+
+     
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
